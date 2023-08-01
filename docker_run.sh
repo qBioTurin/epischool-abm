002c95be9e7d7d97859050eecbc47db45076082f
Daniele Baccega <daniele.baccega@unito.it>
2023-08-01 16:43:08 +0200

    Add --rm to docker-run

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t danielebaccega/reproduce-netlogo:611 .
-docker run --user $UID:$UID -v $(pwd):/home/docker/netlogo danielebaccega/netlogo:611 /usr/bin/bash -c "/home/docker/netlogo/start.sh ${1} ${2}"
+docker run --user $UID:$UID --rm -v $(pwd):/home/docker/netlogo danielebaccega/netlogo:611 /usr/bin/bash -c "/home/docker/netlogo/start.sh ${1} ${2}"
